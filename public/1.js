216823a3ff4bac8711429c5b87126edfceb821d2
Dusty <wajtejprotl@outlook.com>
2020-07-17 22:48:20 +0800

    ajax-test

---
+++ public/1.js
@@ -0,0 +1,17 @@
+console.log('加载js');
+
+
+// const request = new XMLHttpRequest();
+// request.open("GET", "/2.html");
+// request.onreadystatechange = () => {
+//     if (request.readyState === 4 && request.status === 200) {
+
+//         const div = document.createElement("div");
+
+//         div.innerHTML = request.response;
+
+//         document.body.appendChild(div);
+//     }
+// };
+
+// request.send();
